e94e749b6aa411669f4f8e6e8b3db476954511fa
Otz2025 <makolinb@ya.ru>
2025-05-13 19:18:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,15 @@ const CACHE_NAME = 'otz-cache-v1';
 const urlsToCache = [
     '/otz2026/',
     '/otz2026/img/icon-512x512.png',
+    '/otz2026/img/logo.png',
     '/otz2026/Database/featured_matches.json',
     '/otz2026/Database/schedule_data.json',
-    '/otz2026/Database/news_data.json'
+    '/otz2026/Database/news_data.json',
+    '/otz2026/img/Paw Patrol.png',
+    '/otz2026/img/Bulldogs.png',
+    '/otz2026/img/Kitty Boys.png',
+    '/otz2026/img/Megands.png',
+    '/otz2026/img/Crumbl Bishes.png'
 ];
 
 self.addEventListener('install', event => {
